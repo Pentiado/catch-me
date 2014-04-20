a4ba95bb5ca33d0f412b222f1b4245d3c82f45f1
Pawel Wszola <wszola.p@gmail.com>
2014-04-20 17:19:06 +0200

    Emails are passed via socket.io to client

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,15 +6,19 @@ angular.module('catchMeApp', [
   'ngSanitize',
   'ngRoute'
 ])
-  .config(function ($routeProvider, $locationProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'partials/main',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-      
-    $locationProvider.html5Mode(true);
+.config(function ($routeProvider, $locationProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'partials/main',
+      controller: 'MainCtrl'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+  $locationProvider.html5Mode(true);
+}).run(function(){
+  var socket = io.connect('http://localhost');
+  socket.on('email', function (data) {
+    console.log(data);
   });
+});
--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 'use strict';
-// var smtp = require('./stmp_server.js');
 var express = require('express');
-var smtp = require('./smtp_server.js');
+var concat = require('concat-stream');
+var simplesmtp = require('simplesmtp');
+var MailParser = require('mailparser').MailParser;
+var mailparser = new MailParser();
 
-/**
- * Main application file
- */
 
 // Set default node environment to development
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
@@ -20,34 +19,30 @@ require('./lib/routes')(app);
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
+var openSocket;
 io.sockets.on('connection', function (socket) {
   console.log('connection');
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
+  openSocket = socket;
+  // emit all emails
+});
+
+simplesmtp.createSimpleServer({SMTPBanner:'My Server'}, function(req){
+  mailparser.on('end', function(email){
+    console.log('Email:', email);
+    if(openSocket){ openSocket.emit('email', email); }
+    // save to database
   });
+  req.pipe(mailparser);
+  req.accept();
+}).listen(1025, function(err){
+  var message = err ? err.message : 'SMTP server listening on port 1025';
+  console.log(message);
 });
 
 // Start server
 server.listen(config.port, config.ip, function () {
   console.log('Express server listening on %s:%d, in %s mode', config.ip, config.port, app.get('env'));
-
-  smtp.on('startData', function(connection){
-    console.log('Message from:', connection.from);
-    console.log('Message to:', connection.to);
-  });
-
-  smtp.on('data', function(connection, chunk){
-    console.log('data reciving');
-  });
-
-  smtp.on('dataReady', function(connection, callback){
-    console.log('Incoming message saved to /tmp/message.txt');
-    callback(null, 'ABC1');
-  });
-
 });
-
 
 // Expose app
 exports = module.exports = server;
