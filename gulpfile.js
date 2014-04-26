be034060d901398bc720a14c30c209894098e8ff
Pawel Wszola <wszola.p@gmail.com>
2014-04-26 19:31:22 +0200

    Next part of gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,34 @@
 'use strict';
 
 var gulp = require('gulp');
-var plugins = require('gulp-load-plugins')();
+var $ = require('gulp-load-plugins')();
 var http = require('http');
 var openURL = require('open');
 var lazypipe = require('lazypipe');
+var wiredep = require('wiredep').stream;
+var path = require('path');
+var nib = require('nib');
+var config;
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-var config = require('./lib/config/config');
+var testFiles = [
+  'app/bower_components/angular/angular.js',
+  'app/bower_components/angular-mocks/angular-mocks.js',
+  'app/bower_components/angular-resource/angular-resource.js',
+  'app/bower_components/angular-cookies/angular-cookies.js',
+  'app/bower_components/angular-sanitize/angular-sanitize.js',
+  'app/bower_components/angular-route/angular-route.js',
+  'app/scripts/*.js',
+  'app/scripts/**/*.js',
+  'test/mock/**/*.js',
+  'test/spec/**/*.js'
+];
 
 //////////////////////
 // Helper functions //
 //////////////////////
+
 function onServerLog(log) {
-  console.log(plugins.util.colors.white('[') + plugins.util.colors.yellow('nodemon') + plugins.util.colors.white('] ') + log.message);
+  console.log($.util.colors.white('[') + $.util.colors.yellow('nodemon') + $.util.colors.white('] ') + log.message);
 }
 
 function checkAppReady(cb) {
@@ -45,8 +60,25 @@ function whenServerReady (cb) {
 ////////////////////////
 
 var jshint = lazypipe()
-  .pipe(plugins.jshint('.jshintrc'))
-  .pipe(plugins.jshint.reporter, 'jshint-stylish');
+  .pipe($.jshint, '.jshintrc')
+  .pipe($.jshint.reporter, 'jshint-stylish');
+
+var coffeelint = lazypipe()
+  .pipe($.coffeelint)
+  .pipe($.coffeelint.reporter);
+
+var styles = lazypipe()
+  .pipe($.rubySass, {style: 'expanded'})
+  .pipe($.autoprefixer, 'last 1 version')
+  .pipe(gulp.dest, '.tmp/styles');
+
+  // src .less
+  // .pipe($.less({
+  //   paths: [ path.join(__dirname, 'less', 'includes') ]
+  // }))
+
+  // src .styl
+  // .pipe($.stylus, {use: [nib()], errors: true})
 
 /////////
 // CSS //
@@ -54,51 +86,60 @@ var jshint = lazypipe()
 
 gulp.task('clean:css', function () {
   return gulp.src('.tmp/styles', {read: false})
-    .pipe(plugins.clean());
+    .pipe($.clean());
 });
 
 gulp.task('styles', ['clean:css'], function () {
   return gulp.src('app/styles/**/*.scss')
-    .pipe(plugins.sass({
-      includePaths: ['app/bower_components'],
-      errLogToConsole: true
-    }))
-    .pipe(gulp.dest('.tmp/styles'))
-    .pipe(plugins.livereload());
+    .pipe(styles());
 });
 
+
+////////////
+// Coffee //
+////////////
+
+gulp.task('coffee', function() {
+  gulp.src('app/scripts/**/*.coffee')
+    .pipe(coffeelint())
+    .pipe($.coffee({bare: true}).on('error', $.util.log))
+    .pipe(gulp.dest('.tmp/scripts'));
+});
 
 /////////////
 // Linting //
 /////////////
 
-gulp.task('lint', ['jshint:gulp', 'jshint:server', 'jshint:client']);
+gulp.task('lint', ['lint:gulp', 'lint:server', 'lint:client']);
 
-gulp.task('jshint:gulp', function () {
+gulp.task('lint:gulp', function () {
   return gulp.src(['./Gulpfile.js']).pipe(jshint());
 });
 
-gulp.task('jshint:client', function () {
+gulp.task('lint:client', function () {
   return gulp.src(['app/scripts/**/*.js']).pipe(jshint());
 });
 
-gulp.task('jshint:server', function () {
+gulp.task('lint:server', function () {
   return gulp.src([
     'lib/**/*.js',
     'server.js',
-  ]).pipe(jshint);
+  ]).pipe(jshint());
 });
 
 ///////////
 // Start //
 ///////////
 
-gulp.task('start:client', ['start:server', 'jshint:client', 'styles'], function () {
+gulp.task('start:client', ['start:server', 'lint:client', 'styles'], function () {
   openURL('http://localhost:' + config.port);
 });
 
-gulp.task('start:server', ['jshint:server'], function (callback) {
-  plugins.nodemon({
+gulp.task('start:server', ['lint:server'], function (callback) {
+  process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+  config = require('./lib/config/config');
+
+  $.nodemon({
     script: 'server.js',
     watch: ['lib', 'server.js']
   })
@@ -107,17 +148,165 @@ gulp.task('start:server', ['jshint:server'], function (callback) {
 });
 
 gulp.task('watch', function () {
-  gulp.watch('app/styles/**/*.scss', ['styles']);
-  gulp.watch('app/views/**/*.html', function (event) {
-    gulp.src(event.path)
-      .pipe(plugins.livereload());
-  });
-  gulp.watch('app/scripts/**/*.js', function (event) {
-    gulp.src(event.path)
-      .pipe(plugins.jshint('.jshintrc'))
-      .pipe(plugins.jshint.reporter('jshint-stylish'))
-      .pipe(plugins.livereload());
-  });
+
+  $.watch({glob: 'app/styles/**/*.{scss,css}'})
+    .pipe($.plumber())
+    .pipe(styles())
+    .pipe($.livereload());
+
+  $.watch({glob: 'app/views/**/*'})
+    .pipe($.plumber())
+    .pipe($.livereload());
+
+  $.watch({glob: 'app/scripts/**/*.js'})
+    .pipe($.plumber())
+    .pipe(jshint())
+    .pipe($.livereload());
+
+  // $.watch({glob: 'app/scripts/**/*.coffee'})
+  //   .pipe($.plumber())
+  //   .pipe(coffeelint())
+  //   .pipe($.coffee({bare: true}).on('error', $.util.log))
+  //   .pipe(gulp.dest('.tmp/scripts'))
+  //   .pipe($.livereload());
+
+  $.watch({glob: '{app/lib/**/*.js,./*.js,test/**/*.js}'})
+    .pipe($.plumber())
+    .pipe(jshint());
+
+  gulp.watch('bower.json', ['bower']);
 });
 
 gulp.task('serve', ['styles', 'lint', 'start:server', 'start:client', 'watch']);
+
+gulp.task('test:server', function () {
+  process.env.NODE_ENV = 'test';
+  return gulp.src('test/server/**/*.js')
+    .pipe($.mocha({reporter: 'spec'}));
+});
+
+gulp.task('test:client', function () {
+  gulp.src(testFiles)
+    .pipe($.karma({
+      configFile: 'karma.conf.js',
+      action: 'watch'
+    }));
+});
+
+gulp.task('test:client:single', function () {
+  return gulp.src(testFiles)
+    .pipe($.karma({
+      configFile: 'karma.conf.js',
+      action: 'run'
+    })).on('error', function (err) {
+      // Make sure failed tests cause gulp to exit non-zero
+      throw err;
+    });
+});
+
+// inject bower components
+gulp.task('bower', function () {
+  return gulp.src('app/views/index.jade')
+    .pipe(wiredep({
+      directory: './app/bower_components',
+      exclude: ['bootstrap-sass-official']
+    }))
+  .pipe(gulp.dest('app/views/'));
+});
+
+///////////
+// Build //
+///////////
+
+gulp.task('html', function () {
+  var jsFilter = $.filter('**/*.js');
+  var cssFilter = $.filter('**/*.css');
+
+  return gulp.src('app/views/index.jade')
+    .pipe($.jade())
+    .pipe($.useref.assets({searchPath: '{.tmp/}'}))
+    .pipe(jsFilter)
+    .pipe($.uglify())
+    .pipe(jsFilter.restore())
+    .pipe(cssFilter)
+    .pipe($.csso())
+    .pipe(cssFilter.restore())
+    .pipe($.useref.restore())
+    .pipe($.useref())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('images', function () {
+  return gulp.src('app/images/**/*')
+    .pipe($.cache($.imagemin({
+        optimizationLevel: 3,
+        progressive: true,
+        interlaced: true
+    })))
+    .pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('fonts', function () {
+  return $.bowerFiles()
+    .pipe($.filter('**/*.{eot,svg,ttf,woff}'))
+    .pipe($.flatten())
+    .pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('extras', function () {
+  return gulp.src(['app/*.*', '!app/*.html'], { dot: true })
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean:dist', function () {
+  return gulp.src('dist', {read: false}).pipe($.clean());
+});
+
+gulp.task('build', ['clean:dist' ,'html', 'images', 'fonts', 'extras']);
+
+// // // // // // // //
+
+gulp.task('build', ['gulpfile', 'deleteDist', 'copyStatic', 'buildHtml', 'lintClientJs', 'findCssJsFromIndexAndProccess', 'buildServerJs']);
+
+gulp.task('deleteDist', ['gulpfile'], function () {
+  return gulp.src('dist', {read: false})
+    .pipe($.rimraf());
+});
+
+gulp.task('copyStatic', ['gulpfile', 'deleteDist'],  function () {
+  gulp.src(['app/.htaccess', 'app/favicon.ico', 'app/robots.txt'])
+    .pipe(gulp.dest('dist/public'));
+
+  gulp.src(['package.json'])
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('buildHtml', ['gulpfile', 'deleteDist'], function () {
+  gulp.src(['app/views/**/*.*', '!app/views/index.html'])
+    .pipe(gulp.dest('dist/views'));
+
+  return gulp.src('app/views/index.html')
+    .pipe($.useref())
+    .pipe(gulp.dest('dist/views'));
+});
+
+gulp.task('lintClientJs', ['gulpfile', 'deleteDist'], function () {
+  return gulp.src('app/scripts/**/*.js')
+    .pipe($.jshint('.jshintrc')
+    .pipe($.jshint.reporter('jshint-stylish'))
+    .pipe($.jshint.reporter('fail')));
+});
+
+gulp.task('buildServerJs', ['gulpfile', 'deleteDist'], function () {
+  gulp.src(['lib/**/*.js'])
+    .pipe($.jshint('.jshintrc'))
+    .pipe($.jshint.reporter('jshint-stylish'))
+    .pipe($.jshint.reporter('fail'))
+    .pipe(gulp.dest('dist/lib'));
+
+  gulp.src(['server.js'])
+    .pipe($.jshint('.jshintrc'))
+    .pipe($.jshint.reporter('jshint-stylish'))
+    .pipe($.jshint.reporter('fail'))
+    .pipe(gulp.dest('dist'));
+});
--- karma.conf.js
+++ karma.conf.js
@@ -10,18 +10,6 @@ module.exports = function(config) {
     frameworks: ['jasmine'],
 
     // list of files / patterns to load in the browser
-    files: [
-      'app/bower_components/angular/angular.js',
-      'app/bower_components/angular-mocks/angular-mocks.js',
-      'app/bower_components/angular-resource/angular-resource.js',
-      'app/bower_components/angular-cookies/angular-cookies.js',
-      'app/bower_components/angular-sanitize/angular-sanitize.js',
-      'app/bower_components/angular-route/angular-route.js',
-      'app/scripts/*.js',
-      'app/scripts/**/*.js',
-      'test/mock/**/*.js',
-      'test/spec/**/*.js'
-    ],
 
     // list of files / patterns to exclude
     exclude: [],
